583de71bd539cf8ed04e9bd70adb2c1c1d82071d
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-03-24 21:41:05 +0100

    feat: add banner_image_alt text

--- pages/index.js
+++ pages/index.js
@@ -7,34 +7,27 @@ import { FaTransgender, FaBalanceScale } from "react-icons/fa";
 import HomeIcon from "../components/icon_containers/home_icons";
 
 export default function Home({ data }) {
-
-  const { banner_title, banner_subtitle, about_us, banner_image, } = data;
+  const { banner_title, banner_subtitle, about_us, banner_image, banner_image_alt } = data;
 
   return (
     <>
       <div className="h-96 relative">
         <Image
           src={banner_image}
-          alt="Hands raised"
+          alt={banner_image_alt}
           layout="fill"
           objectFit="cover"
           objectPosition="center"
           sizes="(max-width: 575px) 600px, (min-width: 576px) 1200px"
         />
         <div className="absolute inset-0 bg-black bg-opacity-60 page-padding text-white flex flex-col justify-center">
-          <h2 className="text-4xl font-bold w-60 first:text-5xl first:w-96">
-            { banner_title }
-          </h2>
-          <p className="pt-4 text-sm italic w-60 first:text-lg first:w-96">
-            { banner_subtitle }
-          </p>
+          <h2 className="text-4xl font-bold w-60 first:text-5xl first:w-96">{banner_title}</h2>
+          <p className="pt-4 text-sm italic w-60 first:text-lg first:w-96">{banner_subtitle}</p>
         </div>
       </div>
       <div className="page-padding py-12 text-center flex flex-col items-center">
         <h3 className="font-bold italic text-3xl first:text-4xl">A little About Us</h3>
-        <p className="body-text pt-5 max-w-3xl">
-          { about_us }
-        </p>
+        <p className="body-text pt-5 max-w-3xl">{about_us}</p>
         <Link href="/about">
           <a className="link-button mt-5">Learn More</a>
         </Link>
@@ -94,12 +87,12 @@ export default function Home({ data }) {
 }
 
 export async function getStaticProps() {
-  let indexFile = await import('../page_data/index.md');
+  let indexFile = await import("../page_data/index.md");
   let indexData = matter(indexFile.default);
 
   return {
     props: {
       data: indexData.data,
-    }
-  }
+    },
+  };
 }
