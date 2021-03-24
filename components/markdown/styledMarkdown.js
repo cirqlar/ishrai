9363ae96ad66ace7e3d26eba34214f73efbc6f03
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-03-24 22:14:56 +0100

    feat: use forestry data in about page

---
+++ components/markdown/styledMarkdown.js
@@ -0,0 +1,35 @@
+import ReactMarkdown from "react-markdown";
+
+export default function StyledMarkdown({ renderers, ...props }) {
+  const allRenderers = {
+    heading: ({ children, level }) => {
+      switch (level) {
+        case 1:
+          return <h1>{children}</h1>;
+        case 2:
+          return <h2>{children}</h2>;
+        case 3:
+          return <h3 className="font-bold italic text-3xl first:text-4xl">{children}</h3>;
+        case 4:
+          return <h4>{children}</h4>;
+        case 5:
+          return <h5>{children}</h5>;
+        case 6:
+          return <h6>{children}</h6>;
+      }
+    },
+    paragraph: ({ children }) => {
+      return <p className="body-text pt-5 max-w-3xl">{children}</p>;
+    },
+    list: ({ children, ordered }) => {
+      return ordered ? (
+        <ol className="body-list pt-5 max-w-3xl">{children}</ol>
+      ) : (
+        <ul className="body-list pt-5 max-w-3xl">{children}</ul>
+      );
+    },
+    ...renderers,
+  };
+
+  return <ReactMarkdown renderers={allRenderers} {...props} />;
+}
--- pages/about.js
+++ pages/about.js
@@ -1,58 +1,28 @@
+import matter from "gray-matter";
+
 import PageTitle from "../components/layout/pageTitle";
+import StyledMarkdown from "../components/markdown/styledMarkdown";
 import PageHeading from "../components/shared/page_heading";
 
-export default function About() {
+export default function About({ content }) {
   return (
     <>
       <PageTitle title="Who We Are" />
       <PageHeading heading="Who We Are" />
       <div className="page-padding py-12 text-center flex flex-col items-center">
-        <h3 className="font-bold italic text-3xl first:text-4xl">About Us</h3>
-
-        <p className="body-text pt-5 max-w-3xl">
-          Improved Sexual Health and Rights Advocacy Initiative (ISHRAI) was founded in, 2014 and formally registered
-          with Corporate Affairs Commission on the 13th day of January, 2017 with registration number: CAC/IT/NO.93952.
-          ISHRAI was registered to promote the health and rights of marginalized groups in Nigeria through advocacy,
-          active participation, service delivery and innovative engagement processes. Among other things, to advocate
-          for an enabling environment at the community, state and national levels that ensures the functionality of all
-          persons irrespective of age, linguistic or religious affiliation, colour, national or ethnic origin,
-          functional disability, sexual orientation, gender identity/expression, marital status or any other
-          circumstance affecting the private person.
-        </p>
-        <p className="body-text pt-5 max-w-3xl">
-          We are a non-profit, non-governmental, community based organization here in Lagos State, Nigeria. Among other
-          things, to advocate for an enabling environment at the community, state and national levels that ensures the
-          functionality of all persons irrespective of age, linguistic or religious affiliation, colour, national or
-          ethnic origin, functional disability, sexual orientation, gender identity/expression, marital status or any
-          other circumstance affecting the private person.
-        </p>
-        <p className="body-text pt-5 max-w-3xl">
-          Lesbian Gay and Bisexual Persons are forced to deal with the effects of discriminatory legislation which
-          criminalizes their behavior. These laws and resulting fear of discrimination, violence, and arrests further
-          prevent them from accessing health services.
-        </p>
-        <p className="body-text pt-5 max-w-3xl">
-          Not only are Lesbian Gay and Bisexual Person suffering from HIV and Human Rights abuse at a much higher
-          prevalence rate, but they are simultaneously difficult to reach with appropriate interventions. This is
-          because key populations are often mobile, stigmatized, unable to self-identify, marginalized, and at a higher
-          risk of physical abuse. Due to these factors, it is difficult for most organizations to reach key populations
-          with accurate, non-judgmental educational materials that are rights-based and gender sensitive. Specialized
-          and carefully adapted intervention techniques are required. Additionally, the lack of KP-friendly and
-          competent care in public and private health facilities and other service delivery points further increases the
-          vulnerability of key populations by discouraging them from accessing services.
-        </p>
-        <p className="body-text pt-5 max-w-3xl">
-          As a strategy to reach key population, ISHRAI an LGB-led/focused organization have been implementing
-          intervention programs to improve the livelihood of target populations. Key activities include:
-        </p>
-        <ul className="body-list pt-5 max-w-3xl">
-          <li>Capacity building in the area of sexual and human rights for key populations.</li>
-          <li>Centre-based counselling, IEC (Information, Education, Communication) or peer education sessions.</li>
-          <li>Law reforms and policy dialogues at the community, state and national levels.</li>
-          <li>Periodic outreach activities to communities.</li>
-          <li>Consultancy services on sexuality, gender and human rights issues.</li>
-        </ul>
+        <StyledMarkdown children={content} />
       </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  let aboutFile = await import("../page_data/about.md");
+  let aboutData = matter(aboutFile.default);
+
+  return {
+    props: {
+      content: aboutData.content,
+    },
+  };
 }
