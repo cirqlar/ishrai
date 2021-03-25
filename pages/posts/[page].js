17664261691a08235e1f1691e5850a964e5ae0b0
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-03-25 23:59:52 +0100

    feat: add padding

--- pages/posts/[page].js
+++ pages/posts/[page].js
@@ -1,14 +1,18 @@
 import PageHeading from "../../components/shared/page_heading";
 import LinkPagination from "../../components/shared/pagination";
-import { getNumPages, getPage, getPostCount, } from "../../utils/posts";
+import { getNumPages, getPage, getPostCount } from "../../utils/posts";
 
 export default function Posts({ currentPage, pageCount, posts }) {
   return (
     <>
       <PageHeading heading="News/Blog" />
-      <h3 className="font-bold italic text-3xl first:text-4xl">Posts</h3>
-      {posts.map(val => <p key={val.path}>{val.excerpt} post</p>)}
-      <LinkPagination currentPage={currentPage} numPages={pageCount} baseLink="/posts/" />
+      <div className="page-padding py-12 text-center flex flex-col items-center">
+        <h3 className="font-bold italic text-3xl first:text-4xl">Posts</h3>
+        {posts.map((val) => (
+          <p key={val.path}>{val.excerpt} post</p>
+        ))}
+        <LinkPagination currentPage={currentPage} numPages={pageCount} baseLink="/posts/" />
+      </div>
     </>
   );
 }
