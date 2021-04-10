6e203fa9906e91a8ac8cd9939c4784a1e0420310
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2021-04-10 12:45:05 +0100

    feat: style all headings

--- components/markdown/styledMarkdown.js
+++ components/markdown/styledMarkdown.js
@@ -7,15 +7,15 @@ export default function StyledMarkdown({ renderers, ...props }) {
         case 1:
           return <h1 className="font-bold text-4xl first:text-5xl">{children}</h1>;
         case 2:
-          return <h2>{children}</h2>;
+          return <h2 className="font-bold italic text-3xl first:text-4xl">{children}</h2>;
         case 3:
           return <h3 className="font-bold italic text-3xl first:text-4xl">{children}</h3>;
         case 4:
           return <h4 className="font-bold text-2xl first:text-3xl">{children}</h4>;
         case 5:
-          return <h5>{children}</h5>;
+          return <h5 className="font-bold text-xl first:text-2xl">{children}</h5>;
         case 6:
-          return <h6>{children}</h6>;
+          return <h6 className="text-base font-bold">{children}</h6>;
       }
     },
     paragraph: ({ children }) => {
